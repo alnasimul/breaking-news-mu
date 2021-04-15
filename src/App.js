fbb01a44b446d08f0a587c2aa7b25cd7b0823136
Al Nasimul Haque <alnasimul@gmail.com>
2021-04-15 17:11:03 +0600

    data loaded using axios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { useEffect, useState } from 'react';
 import News from './components/News/News';
+import axios from 'axios';
 
 function App() {
   const [articles,setArticles] = useState([]);
+  // useEffect(() => {
+  //   const url = 'https://newsapi.org/v2/everything?q=tesla&from=2021-03-15&sortBy=publishedAt&apiKey=bafe0adad4714346881cd6730410d3af';
+  //   fetch(url)
+  //   .then(res => res.json())
+  //   .then(data => console.log(data))
+  // },[])
+
   useEffect(() => {
     const url = 'https://newsapi.org/v2/everything?q=tesla&from=2021-03-15&sortBy=publishedAt&apiKey=bafe0adad4714346881cd6730410d3af';
-    fetch(url)
-    .then(res => res.json())
-    .then(data => setArticles(data.articles))
+    axios(url)
+    .then(data => setArticles(data.data.articles))
   },[])
   return (
     <div className="App">
